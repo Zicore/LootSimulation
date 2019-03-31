267fb0f986ee4fc252eb03b1dad72beba447d955
Zicore <zicore@live.de>
2019-03-31 19:19:18 +0200

    remove commented code

--- src/modules/loot/Looter.ts
+++ src/modules/loot/Looter.ts
@@ -29,38 +29,4 @@ export class Looter {
 
         return result;
     }
-
-    // public loot(timesToLoot:number) {
-    //     this.lootCount = {};
-    //     for (let i: number = 0; i < timesToLoot; i++) {
-    //         let item = this.lootTable.loot();
-    //         if (item != null) {
-    //             if (this.lootCount[item.name] == null) {
-    //                 this.lootCount[item.name] = new LootCountItem(item);
-    //             }
-    //             this.lootCount[item.name].incrementLootCount();
-    //         }
-    //     }
-
-    //     if (Object.keys(this.lootCount).length > 0) {
-    //         console.log(`You looted ${timesToLoot} times. This is the distribution: `)
-    //     } else {
-    //         console.log(`You looted ${timesToLoot} times, but you didn't find anything.`);
-    //     }
-
-    //     let sumCount: number = 0;
-    //     let sumProbability: number = 0;
-    //     for (let item of this.lootTable.items) {
-    //         if (this.lootCount[item.name] != null) {
-    //             let lootCountItem = this.lootCount[item.name];
-    //             console.log(`Item: ${item.name} Count: ${lootCountItem.count} Probability: ${lootCountItem.item.probability}% Actual Distribution: ${(lootCountItem.count / timesToLoot * this.lootTable.maxProbability).toFixed(2)}%`);
-    //             sumCount += lootCountItem.count;
-    //             sumProbability += lootCountItem.item.probability;
-    //         }
-    //     }
-    //     let sumNothingFound: number = timesToLoot - sumCount;
-    //     console.log(`${sumNothingFound} times nothing was found. Probability: ${(this.lootTable.emptyProbability()).toFixed(2)}% Actual Distribution: ${(sumNothingFound / timesToLoot * this.lootTable.maxProbability).toFixed(2)}%`);
-
-    //     return new LootResult(this.lootTable, this.lootCount, sumNothingFound);
-    // }
 }
